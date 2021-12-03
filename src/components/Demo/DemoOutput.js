c7c519fd3713aa4f8c5a852f0805d6b2da53a77e
Sealman <castle2668@gmail.com>
2021-12-03 17:06:52 +0800

    Child Component Re-Evaluation

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import MyParagraph from "./MyParagraph";
+
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
 
-  return <p>{props.show ? "This is new!" : ""}</p>;
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
 export default DemoOutput;
